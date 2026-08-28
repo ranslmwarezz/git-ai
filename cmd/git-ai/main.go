4d04d396bd4b748414c0f05564f074d2eb775841
Ranslmware <ranslmware40@gmail.com>
2026-08-28 15:37:02 -0300

    feat: adiciona comando commit ao CLI

--- cmd/git-ai/main.go
+++ cmd/git-ai/main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	
+
+	args := os.Args
+
+	if len(args) < 2 {
+		fmt.Println("Uso: git-ai <comando>")
+		return
+	}
+
+	command := args[1]
+
+	switch command {
+	case "commit":
+		fmt.Println("Comando commit executado!")
+	default: 
+		fmt.Printf("Comando desconhecido: %s\n", command)
+		
+	}
 }
