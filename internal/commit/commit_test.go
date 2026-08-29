e1ba9ec35ae2f87516ba5584930ea4b117c61a90
Ranslmware <ranslmware40@gmail.com>
2026-08-29 03:31:46 -0300

    test: atualiza testes do commit service

--- internal/commit/commit_test.go
+++ internal/commit/commit_test.go
@@ -14,38 +14,55 @@ func (f *fakeGit) DiffCached() (string, error) {
 	return f.diff, f.err
 }
 
-func TestReturnsDiff(t *testing.T) {
-	expectedDiff := "diff de teste"
+type fakeAI struct {
+	message string
+	err     error
+	called  bool
+}
 
-	fake := &fakeGit{
-		diff: expectedDiff,
+func (a *fakeAI) GenerateCommitMessage(diff string) (string, error) {
+	a.called = true
+	return a.message, a.err
+}
+
+func TestRunReturnsCommitMessage(t *testing.T) {
+	fakeGit := &fakeGit{
+		diff: "diff de teste",
 	}
 
-	service := NewService(fake)
+	expectedMessage := "feat: adiciona teste"
 
-	diff, err := service.Run()
+	fakeAi := &fakeAI{
+		message: expectedMessage,
+	}
+
+	service := NewService(fakeGit, fakeAi)
+
+	message, err := service.Run()
 
 	if err != nil {
 		t.Fatalf("erro inesperado: %v", err)
 	}
 
-	if diff != expectedDiff {
-		t.Errorf("diff esperado %q, obtido: %q", expectedDiff, diff)
+	if message != expectedMessage {
+		t.Fatalf("messagem esperada %q, obitida: %q", expectedMessage, message)
 	}
 }
 
 func TestReturnsError(t *testing.T) {
 	expectedError := errors.New("erro ao executar git")
 
-	fake := &fakeGit{
+	fakeGit := &fakeGit{
 		err: expectedError,
 	}
 
-	service := NewService(fake)
+	fakeAi := &fakeAI{}
+
+	service := NewService(fakeGit, fakeAi)
 
 	_, err := service.Run()
 
-	// se o erro for diferente do que qual espero o if é acionado
+	// se o erro for diferente do que espero, o if é acionado
 	if err != expectedError {
 		t.Errorf("erro esperado %v, obtido: %v", expectedError, err)
 	}
@@ -53,11 +70,13 @@ func TestReturnsError(t *testing.T) {
 
 func TestReturnsEmptyDiff(t *testing.T) {
 
-	fake := &fakeGit{
+	fakeGit := &fakeGit{
 		diff: "",
 	}
 
-	service := NewService(fake)
+	fakeAi := &fakeAI{}
+
+	service := NewService(fakeGit, fakeAi)
 
 	diff, err := service.Run()
 
@@ -67,5 +86,33 @@ func TestReturnsEmptyDiff(t *testing.T) {
 
 	if diff != "" {
 		t.Errorf("esperava diff vazio, obtido: %q", diff)
+	}
+
+	if fakeAi.called {
+		t.Errorf("a IA não deveria ser chamada")
+	}
+}
+
+func TestReturnsAIError(t *testing.T) {
+	expectedError := errors.New("erro ao gerar mensagem")
+
+	fakeGit := &fakeGit{
+		diff: "diff teste",
+	}
+
+	fakeAI := &fakeAI{
+		err: expectedError,
+	}
+
+	service := NewService(fakeGit, fakeAI)
+
+	_, err := service.Run()
+
+	if err != expectedError {
+		t.Errorf("erro esperado %v, obtido: %v", expectedError, err)
+	}
+
+	if !fakeAI.called {
+		t.Errorf("a IA deveria ter sido chamada")
 	}
 }
