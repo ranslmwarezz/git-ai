1c90d6f90d6e5935db47ecc7d834e3ea16c5e4cf
Ranslmware <ranslmware40@gmail.com>
2026-09-08 02:08:15 -0300

    feat(tui): integra confirmação ao fluxo de commit

--- cmd/git-ai/main.go
+++ cmd/git-ai/main.go
@@ -5,8 +5,10 @@ import (
 	"git-ai/internal/ai"
 	"git-ai/internal/commit"
 	"git-ai/internal/git"
+	"git-ai/internal/tui"
 	"os"
 
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/joho/godotenv"
 )
 
@@ -35,25 +37,46 @@ func main() {
 
 	switch command {
 	case "commit":
-		gitClient := &git.Client{}
+		gitClient := git.NewClient()
 		aiClient := ai.NewAPIClient("https://generativelanguage.googleapis.com/v1beta/interactions",
 			apiKey)
 		commitService := commit.NewService(gitClient, aiClient)
 
-		diff, err := commitService.Run()
+		message, err := commitService.Run()
 
 		if err != nil {
 			fmt.Println("Erro: ", err)
 			return
 		}
 
-		if diff == "" {
+		if message == "" {
 			fmt.Println("Nenhuma alteração encontrada no staging.")
 			fmt.Println("Execute: git add <arquivo>")
 			return
 		}
 
-		fmt.Println(diff)
+		model := tui.NewModel(message)
+
+		program := tea.NewProgram(model)
+
+		finalModel, err := program.Run()
+		if err != nil {
+			fmt.Println("Erro ao inicar TUI:", err)
+			return
+		}
+
+		final := finalModel.(tui.Model)
+
+		if !final.Confirmed() {
+			fmt.Println("Commit cancelado.")
+			return
+		}
+
+		if err := gitClient.Commit(message); err != nil {
+			fmt.Println("Erro ao executar commit:", err)
+			return
+		}
+		fmt.Println("Commit realizado.")
 
 	default:
 		fmt.Printf("Comando desconhecido: %s\n", command)
